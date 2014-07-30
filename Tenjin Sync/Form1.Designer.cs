61f2372819cbfbacb5ef3cb46d1a23269b590df2
nosedive25 <nosedive25@git.git>
2014-07-29 22:30:49 -0700

    Initial Upload

---
+++ Tenjin Sync/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Tenjin_Sync
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.main = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // main
+            // 
+            this.main.AutoSize = true;
+            this.main.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.main.Location = new System.Drawing.Point(99, 46);
+            this.main.Name = "main";
+            this.main.Size = new System.Drawing.Size(213, 25);
+            this.main.TabIndex = 0;
+            this.main.Text = "Syning with OneNote";
+            this.main.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 132);
+            this.Controls.Add(this.main);
+            this.Name = "Form1";
+            this.Text = "Tenjin Sync";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label main;
+    }
+}
+
---
+++ Tenjin Sync/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Microsoft.Office.Interop.OneNote;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+/*Due to the imperfect nature of Tenjin Sync (as well as the limited methods of syncing OneNote), it's recommended to run it as its own user.*/
+namespace Tenjin_Sync
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+        [DllImport("user32.dll", EntryPoint = "FindWindow")]
+        private static extern int FindWindow(string sClass, bool sWindow);
+        
+        //The path to publish the XML feeds to (Usually somewhere in the IIS or Apache folder, ensure the user has write permissions)
+        String iisPath = "C:\\inetpub\\wwwroot\\onenote\\";
+
+        //Full names, as registered in their respective Microsoft accounts
+        String roommate1 = "";
+        String roommate2 = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            refreshOnenote();
+        }
+
+        public void readHomework()
+        {
+            var onenoteApp = new Microsoft.Office.Interop.OneNote.Application();
+
+            string notebookXml;
+            onenoteApp.GetHierarchy(null, HierarchyScope.hsPages, out notebookXml);
+
+            var doc = XDocument.Parse(notebookXml);
+            var ns = doc.Root.Name.Namespace;
+            foreach (var notebookNode in from node in doc.Descendants(ns + "Notebook") select node)
+            {
+                if (notebookNode.Attribute("name").Value == "Tenjin")
+                {
+                    foreach (var sectionNode in from node in notebookNode.Descendants(ns + "Section") select node)
+                    {
+                        foreach (var pageNode in from node in sectionNode.Descendants(ns + "Page") select node)
+                        {
+                            if (pageNode.Attribute("name").Value == roommate1.Split(' ')[0])
+                            {
+                                string hwxml;
+                                onenoteApp.GetPageContent(pageNode.Attribute("ID").Value, out hwxml);
+                                System.IO.File.WriteAllText(iisPath + roommate1.Split(' ')[0] + ".xml", XDocument.Parse(hwxml).ToString().Replace(roommate1, "").Replace(roommate1.Split(' ')[0], ""));
+                            }
+                            if (pageNode.Attribute("name").Value == roommate2.Split(' ')[0])
+                            {
+                                string hwxml;
+                                onenoteApp.GetPageContent(pageNode.Attribute("ID").Value, out hwxml);
+                                System.IO.File.WriteAllText(iisPath + roommate2.Split(' ')[0] + ".xml", XDocument.Parse(hwxml).ToString().Replace(roommate2, "").Replace(roommate2.Split(' ')[0], ""));
+                            }
+                        }
+                    }
+                }
+            }
+
+            refreshOnenote();
+        }
+
+        public void refreshOnenote()
+        {
+            Process.Start("C:\\Program Files\\Microsoft Office\\Office15\\ONENOTE.EXE");
+            BringWindowToTop("Homework - Tenjin", false);
+
+            System.Windows.Forms.Timer syncTimer = new System.Windows.Forms.Timer();
+            syncTimer.Tick += new EventHandler(killOnenote);
+            syncTimer.Interval = 1000 * 60 * 2;
+            syncTimer.Enabled = true;
+            syncTimer.Start();
+        }
+
+        public void killOnenote(Object source, EventArgs e)
+        {
+            foreach (Process proc in Process.GetProcessesByName("ONENOTE"))
+            {
+                proc.Kill();
+            }
+
+            readHomework();
+        }
+
+        public static bool BringWindowToTop(string windowName, bool wait)
+        {
+            int hWnd = FindWindow(windowName, wait);
+            if (hWnd != 0)
+            {
+                return SetForegroundWindow((IntPtr)hWnd);
+            }
+
+            return false;
+        }
+    }
+}
